b153f0292f901bb43c63002d808aa5e0a0dce7ef
Quillraven <herf@gmx.at>
2019-04-30 16:11:41 +0200

    Starting point of tutorial (plain Kotlin)

--- core/src/com/libktx/game/Game.kt
+++ core/src/com/libktx/game/Game.kt
@@ -1,59 +1,28 @@
 package com.libktx.game
 
-import com.badlogic.gdx.assets.AssetManager
-import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.utils.viewport.FitViewport
-import com.badlogic.gdx.utils.viewport.Viewport
-import com.libktx.game.screen.LoadingScreen
-import ktx.app.KtxGame
-import ktx.app.KtxScreen
-import ktx.inject.Context
-import ktx.log.logger
+import com.libktx.game.screen.MainMenuScreen
 
-class Game : KtxGame<KtxScreen>() {
-    private val log = logger<Game>()
-
-    private val context = Context()
-    private val viewport = FitViewport(800f, 480f)
-    private val batch by lazy { SpriteBatch() }
+class Game : com.badlogic.gdx.Game() {
+    public lateinit var batch: SpriteBatch
+    public lateinit var font: BitmapFont
 
     override fun create() {
-        // creating the game context
-        // context contains all instances that are relevant for the entire game
-        // Note: context.dispose() will also dispose all instances that extended 'Disposable' interface
-        context.register {
-            bindSingleton<Batch>(batch)
-            bindSingleton<Viewport>(viewport)
-            bindSingleton(BitmapFont())
-            bindSingleton(AssetManager())
-            bindSingleton(this@Game)
-
-            // create LoadingScreen that will be responsible to load all assets of the game
-            addScreen(LoadingScreen(inject(), inject<Viewport>().camera, inject(), inject(), inject()))
-        }
-
-        // first screen is our LoadingScreen
-        setScreen<LoadingScreen>()
-        super.create()
+        batch = SpriteBatch()
+        // use LibGDX's default Arial font
+        font = BitmapFont()
+        this.setScreen(MainMenuScreen(this))
     }
 
     override fun render() {
-        // always update camera and set batch projection matrix as this is needed for all screens
-        viewport.apply()
-        batch.projectionMatrix = viewport.camera.combined
-        super.render()
-    }
-
-    override fun resize(width: Int, height: Int) {
-        log.debug { "Resizing to $width x $height" }
-        viewport.update(width, height, true)
-        super.resize(width, height)
+        super.render()  // important!
     }
 
     override fun dispose() {
-        context.dispose()
-        super.dispose()
+        this.getScreen().dispose()
+
+        batch.dispose()
+        font.dispose()
     }
 }
