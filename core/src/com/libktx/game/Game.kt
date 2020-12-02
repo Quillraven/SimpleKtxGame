e24b52e8dfa1e88a31fbd44aae91e98a4e5e24a5
Quillraven <quillraven@gmail.com>
2020-12-02 20:39:36 +0100

    fix an endless loop bug in dispose

--- core/src/com/libktx/game/Game.kt
+++ core/src/com/libktx/game/Game.kt
@@ -9,13 +9,13 @@ import com.libktx.game.screen.LoadingScreen
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.inject.Context
+import ktx.inject.register
 
 class Game : KtxGame<KtxScreen>() {
     private val context = Context()
 
     override fun create() {
         context.register {
-            bindSingleton(this@Game)
             bindSingleton<Batch>(SpriteBatch())
             bindSingleton(BitmapFont())
             bindSingleton(AssetManager())
@@ -23,7 +23,7 @@ class Game : KtxGame<KtxScreen>() {
             //    pixels no matter what the screen resolution is.
             bindSingleton(OrthographicCamera().apply { setToOrtho(false, 800f, 480f) })
 
-            addScreen(LoadingScreen(inject(), inject(), inject(), inject(), inject()))
+            addScreen(LoadingScreen(this@Game, inject(), inject(), inject(), inject()))
         }
         setScreen<LoadingScreen>()
         super.create()
